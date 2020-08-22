a85d34af02b22fc748aac2df4e1b0f1a261c0054
QSC <toufictalib@gmail.com>
2020-08-22 09:09:37 +0300

    Exception Handler

---
+++ src/main/java/com/test/springbootproject/exception/RestExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.test.springbootproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+	private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	protected ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+		ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage());
+		return buildResponseEntity(apiError);
+	}
+	
+	@ExceptionHandler(value 
+		      = { IllegalArgumentException.class, IllegalStateException.class })
+	protected ResponseEntity<Object> handleIllegalArgumentException(RuntimeException ex) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_GATEWAY, ex.getMessage());
+		return buildResponseEntity(apiError);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<Object> handleException(Exception ex) {
+		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+		return buildResponseEntity(apiError);
+	}
+}
